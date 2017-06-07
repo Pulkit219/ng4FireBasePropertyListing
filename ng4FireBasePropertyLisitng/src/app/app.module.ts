0e9c278f75acb7ca124a8354cf317caed138772d
Pulkit219 <ascension1110@gmail.com>
2017-06-07 12:50:30 -0400

    4th comm

--- ng4FireBasePropertyLisitng/src/app/app.module.ts
+++ ng4FireBasePropertyLisitng/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,6 +12,12 @@ import { ListingComponent } from './components/listing/listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 
+const appRoutes: Routes = [
+ {path:'' , component:HomeComponent},
+  {path:'listings' , component:ListingsComponent}
+  
+
+];
 
 @NgModule({
   declarations: [
@@ -26,7 +33,8 @@ import { AddListingComponent } from './components/add-listing/add-listing.compon
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
